d5d771c08b73404930218d6e058670f15f38a2da
dovla2001 <vmandic83@gmail.com>
2023-01-12 13:26:44 +0100

    Novi korisnici

---
+++ Korisnici novi.sql
@@ -0,0 +1,59 @@
+insert into spent_energy_meter (customer, streetName, spent_total, streetNumber, city, state)
+values ('Jovan Ivkovic','Radnicka',99,4,'Novi Sad','Srbija');
+
+insert into spent_energy_meter (customer, streetName, spent_total, streetNumber, city, state)
+values ('Marko Nikolic','Pobednicka',74,12,'Beograd','Srbija');
+
+insert into spent_energy_meter (customer, streetName, spent_total, streetNumber, city, state)
+values ('Jovan Okiljevic','Kralja Petra',0,15,'Sabac','Srbija');
+
+insert into spent_energy_meter (customer, streetName, spent_total, streetNumber, city, state)
+values ('Petar Milanovic','Brece Tomica',0,42,'Vrbas','Srbija');
+
+insert into spent_energy_meter (customer, streetName, spent_total, streetNumber, city, state)
+values ('Nikolina Nesic','Pap Pavla',0,11,'Kragujevac','Srbija');
+
+insert into spent_energy_meter (customer, streetName, spent_total, streetNumber, city, state)
+values ('Nina Petric','Bulevar Evrope',45,59,'Novi Sad','Srbija');
+
+insert into spent_energy_meter (customer, streetName, spent_total, streetNumber, city, state)
+values ('Jovan Bratic','Bulevar Slobode',14,7,'Beograd','Srbija');
+
+insert into spent_energy_meter (customer, streetName, spent_total, streetNumber, city, state)
+values ('Blagota Vukajlovic','Petra Bigina',44,21,'Sombor','Srbija');
+
+insert into spent_energy_meter (customer, streetName, spent_total, streetNumber, city, state)
+values ('Vanja Okiljevic','Petra Drapsina',0,55,'Subotica','Srbija');
+
+insert into spent_energy_meter (customer, streetName, spent_total, streetNumber, city, state)
+values ('Predrag Despotovic','Napredna',41,81,'Nis','Srbija');
+
+insert into spent_energy_meter (customer, streetName, spent_total, streetNumber, city, state)
+values ('Marija Stijelja','Bulevar Cara Lazara',45,49,'Novi Sad','Srbija');
+
+insert into spent_energy_meter (customer, streetName, spent_total, streetNumber, city, state)
+values ('Veronika Keric','Bulevar Cara Dusana',0,37,'Beograd','Srbija');
+
+insert into spent_energy_meter (customer, streetName, spent_total, streetNumber, city, state)
+values ('Luka Lukic','Bulevar Oslobodenja',0,92,'Novi Sad','Srbija');
+
+insert into spent_energy_meter (customer, streetName, spent_total, streetNumber, city, state)
+values ('Marija Bulatovic','Bulevar Slobode',42,11,'Kragujevac','Srbija');
+
+insert into spent_energy_meter (customer, streetName, spent_total, streetNumber, city, state)
+values ('Cedo Vukovic','Jovana Jovanovica Zmaja',32,33,'Zrenjanin','Srbija');
+
+insert into spent_energy_meter (customer, streetName, spent_total, streetNumber, city, state)
+values ('Kristina Ketic','Vuka Karadica',0,2,'Sombor','Srbija');
+
+insert into spent_energy_meter (customer, streetName, spent_total, streetNumber, city, state)
+values ('Nikola Radovic','Jovana Cvijica',33,67,'Subotica','Srbija');
+
+insert into spent_energy_meter (customer, streetName, spent_total, streetNumber, city, state)
+values ('Marko Becic','Bulevar Cara Lazara',0,39,'Beograd','Srbija');
+
+insert into spent_energy_meter (customer, streetName, spent_total, streetNumber, city, state)
+values ('Marija Maric','Beogradska',0,19,'Leskovac','Srbija');
+
+insert into spent_energy_meter (customer, streetName, spent_total, streetNumber, city, state)
+values ('Vladimir Putin','Josipa Broza Tita',45,3,'Pancevo','Srbija');
